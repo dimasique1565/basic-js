0db8433048198edcd5173d52f7543afe79de514d
dimasique1565 <velkov2000@list.ru>
2020-10-12 03:08:23 +0300

    feat: add vigenere task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,14 +5,68 @@ class VigenereCipheringMachine {
     this.directMode = dir_mode;
   }
   encrypt(message, key) {
-    throw new CustomError('Not implemented');
     if(typeof message != "string" || typeof key != "string") throw Error;
-    message = message.toUpperCase;
+    let alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    message = message.toUpperCase();
+    let keyarr = key.toUpperCase().split('');
+    let msgarr = message.split('');
+
+    let onlyletters = [];
+    msgarr.forEach(function(elem){
+      if(alphabetArray.includes(elem)) {
+        onlyletters.push(elem);
+      }
+    });
+
+    let keyarrlon = [];
+    for(let i = 0, j = 0; i < onlyletters.length; i++, j++) {
+      if(j >= keyarr.length) j -= keyarr.length;
+      keyarrlon.push(keyarr[j]);
+    }
+
+    for(let i = 0; i < onlyletters.length; i++) {
+      let num = onlyletters[i].charCodeAt() + keyarrlon[i].charCodeAt() - 130;
+      onlyletters[i] = alphabetArray[num % 26];
+    }
+    
+    for(let i = 0, j = 0; i < msgarr.length; i++) {
+      if(alphabetArray.includes(msgarr[i])) {
+        msgarr[i] = onlyletters[j];
+        j++;
+      }
+    }
+    return this.directMode? msgarr.join('') : msgarr.reverse().join('');
   }
   decrypt(encryptedMessage, key) {
-    throw new CustomError('Not implemented');
     if(typeof encryptedMessage != "string" || typeof key != "string") throw Error;
+    let alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    let keyarr = key.toUpperCase().split('');
+    let msgarr = encryptedMessage.split('');
+    let onlyletters = [];
+    msgarr.forEach(function(elem){
+      if(alphabetArray.includes(elem)) {
+        onlyletters.push(elem);
+      }
+    });
+
+    let keyarrlon = [];
+    for(let i = 0, j = 0; i < onlyletters.length; i++, j++) {
+      if(j >= keyarr.length) j -= keyarr.length;
+      keyarrlon.push(keyarr[j]);
+    }
+
+    for(let i = 0; i < onlyletters.length; i++) {
+      let num = onlyletters[i].charCodeAt() - keyarrlon[i].charCodeAt() + 26;
+      onlyletters[i] = alphabetArray[num % 26];
+    }
     
+    for(let i = 0, j = 0; i < msgarr.length; i++) {
+      if(alphabetArray.includes(msgarr[i])) {
+        msgarr[i] = onlyletters[j];
+        j++;
+      }
+    }
+    return this.directMode? msgarr.join('') : msgarr.reverse().join('');
   } 
 }
 
